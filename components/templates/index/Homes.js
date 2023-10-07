5da43b08e9e0b1ac3ec11f65b711a3e303aa6156
reyhane <reyhanehalipour78@gmail.com>
2023-10-07 18:53:32 +0330

    first commit

---
+++ components/templates/index/Homes.js
@@ -0,0 +1,17 @@
+import React from "react";
+import db from "./../../../data/db.json";
+import Home from "@/components/modules/Home";
+
+function Homes() {
+
+  console.log(db);
+  return (
+    <div class="homes">
+    {db.homes.slice(0, 6).map((home) => (
+      <Home key={home.id} {...home} />
+    ))}
+  </div>
+  );
+}
+
+export default Homes;
